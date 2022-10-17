a685e4634c462859be90b48d5f7efed7344ac8cf
eighteengray <eighteengray@163.com>
2022-10-17 08:53:35 +0800

    first commit

---
+++ android/app/src/main/java/com/eighteengray/baseproject/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.eighteengray.baseproject
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/java/com/eighteengray/baseproject/MainApplication.kt
@@ -0,0 +1,13 @@
+package com.eighteengray.baseproject
+
+import android.app.Application
+
+
+class MainApplication: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+}
